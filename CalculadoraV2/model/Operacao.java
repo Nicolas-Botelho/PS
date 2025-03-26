015d5d79d5abdb43cb270d10db4a212796301a9d
Nicolas-Botelho <125471140+Nicolas-Botelho@users.noreply.github.com>
2025-03-26 19:07:19 -0300

    Delete CalculadoraV2/model directory

--- CalculadoraV2/model/Operacao.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public interface Operacao {
-
-    public abstract double operacao(double n1, double n2);
-}
--- CalculadoraV2/model/Soma.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Soma implements Operacao{
-
-    @Override
-    public double operacao(double n1, double n2) {
-        return n1 + n2;
-    }
-}
